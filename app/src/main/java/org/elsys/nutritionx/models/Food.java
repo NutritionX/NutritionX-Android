803fcf3f3dab581a369b893ddca92a54ee08f12f
Dimitar Nikolov <dimitar@doonloo.com>
2018-04-01 19:03:43 +0300

    Create food class

---
+++ app/src/main/java/org/elsys/nutritionx/models/Food.java
@@ -0,0 +1,81 @@
+package org.elsys.nutritionx.models;
+
+import java.util.Objects;
+
+public class Food {
+    private String name;
+    private Float calories;
+    private Float protein;
+    private Float carbs;
+    private Float fats;
+
+    public Food() {
+
+    }
+
+    public Food(String name, Float calories, Float protein, Float carbs, Float fats) {
+        this.name = name;
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fats = fats;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Float calories) {
+        this.calories = calories;
+    }
+
+    public Float getProtein() {
+        return protein;
+    }
+
+    public void setProtein(Float protein) {
+        this.protein = protein;
+    }
+
+    public Float getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(Float carbs) {
+        this.carbs = carbs;
+    }
+
+    public Float getFats() {
+        return fats;
+    }
+
+    public void setFats(Float fats) {
+        this.fats = fats;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Food food = (Food) o;
+        return Objects.equals(name, food.name) &&
+                Objects.equals(calories, food.calories) &&
+                Objects.equals(protein, food.protein) &&
+                Objects.equals(carbs, food.carbs) &&
+                Objects.equals(fats, food.fats);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, calories, protein, carbs, fats);
+    }
+}
