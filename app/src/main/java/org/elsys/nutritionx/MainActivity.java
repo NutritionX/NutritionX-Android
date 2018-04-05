b96301dd8204e325480e4d9431b63a8c3ca39c8a
Dimitar Nikolov <dimitar@doonloo.com>
2018-04-05 14:36:59 +0300

    Add Autocomplete library to project and initialize views

--- app/src/main/java/org/elsys/nutritionx/MainActivity.java
+++ app/src/main/java/org/elsys/nutritionx/MainActivity.java
@@ -1,52 +1,83 @@
 package org.elsys.nutritionx;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.otaliastudios.autocomplete.Autocomplete;
+import com.otaliastudios.autocomplete.AutocompletePolicy;
+import com.otaliastudios.autocomplete.AutocompletePresenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mEnterFoodLabel;
+    private EditText mEnterFoodEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mEnterFoodLabel = findViewById(R.id.enter_food_label);
+        mEnterFoodEditText = findViewById(R.id.enter_food_edit_text);
+
+        AutocompletePolicy policy = new AutocompletePolicy() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean shouldShowPopup(Spannable text, int cursorPos) {
+                return false;
             }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            @Override
+            public boolean shouldDismissPopup(Spannable text, int cursorPos) {
+                return false;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public CharSequence getQuery(Spannable text) {
+                return null;
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void onDismiss(Spannable text) {
 
-        return super.onOptionsItemSelected(item);
+            }
+        };
+
+        AutocompletePresenter presenter = new AutocompletePresenter(this) {
+            @Override
+            protected ViewGroup getView() {
+                return null;
+            }
+
+            @Override
+            protected void onViewShown() {
+
+            }
+
+            @Override
+            protected void onQuery(@Nullable CharSequence query) {
+
+            }
+
+            @Override
+            protected void onViewHidden() {
+
+            }
+        };
+
+        Autocomplete.on(mEnterFoodEditText)
+                .with(policy)
+                .with(presenter)
+                .build();
     }
 }
